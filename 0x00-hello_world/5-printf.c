1db59fa00aca660ea86db60fc232c772c9773d34
Elias1011 <johnelias720@gmail.com>
2023-03-05 11:23:31 -0800

    Hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - this is the main function
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
-        puts("\"with proper grammar, but the outcome is a piece of art,
-\n");
+        puts("\"with proper grammar, but the outcome is a piece of art,\n");
     return (0);
 }
